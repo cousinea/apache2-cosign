e50ded0490a8c2027e1babeb2369c1680467d47f
chriskretler <ckretler@umich.edu>
2017-02-03 16:27:13 -0500

    change location of expected log files.

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ ln -s /usr/local/apache2/local/conf/httpd.conf /usr/local/apache2/conf/httpd.con
 
 ln -s /usr/local/apache2/local/conf/httpd-cosign.conf /usr/local/apache2/conf/extra/httpd-cosign.conf
 
-ln -s /proc/self/fd/1 /var/log/apache2/access.log
-ln -s /proc/self/fd/1 /var/log/apache2/error.log
+ln -s /proc/self/fd/1 /usr/local/apache2/logs/access.log
+ln -s /proc/self/fd/1 /usr/local/apache2/logs/error.log
 
 /usr/local/apache2/bin/httpd -DFOREGROUND 

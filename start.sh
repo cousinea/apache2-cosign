650a14db78c53cadde512e71eef00ace43387415
chriskretler <ckretler@umich.edu>
2017-02-07 16:04:39 -0500

    add c_rehash to start script.

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ ln -sf /usr/local/apache2/local/conf/httpd.conf /usr/local/apache2/conf/httpd.co
 ln -s /usr/local/apache2/local/conf/httpd-cosign.conf /usr/local/apache2/conf/extra/httpd-cosign.conf
 
 # Rehash command needs to be run before starting apache.
-# c_rehash /usr/local/apache2/local/certs
+c_rehash /usr/local/apache2/local/certs
 
 # Redirect logs to stdout and stderr for docker reasons.
 ln -sf /dev/stdout /usr/local/apache2/logs/access_log
